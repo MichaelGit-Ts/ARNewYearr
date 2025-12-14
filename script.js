44a648e340406f1a52c4264e8dbf5bf7279abb0d
Mikhail Tsymlyakov <tsymlyakovmihon2009@yandex.ru>
2025-12-15 00:23:16 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const button = document.getElementById("ShotButton")
-button.addEventListener("click", function() {
-    console.log("Клик");
-    alert("Привет")
+const screenshotBtn = document.getElementById('ShotButton');
+const scene = document.querySelector('a-scene');
+
+screenshotBtn.addEventListener('click', function () {
+    scene.components.screenshot.capture('perspective')
+        .then(function (dataURI) {
+            const link = document.createElement('a');
+            link.download = 'screenshot-' + Date.now() + '.png';
+            link.href = dataURI;
+            link.click();
+
+        })
+        .catch(function (error) {
+            console.error('Ошибка при создании скриншота:', error);
+        });
 });
